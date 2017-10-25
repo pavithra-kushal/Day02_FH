6ee5af06b946f7a1f0fead0175c0de6f6b011a83
K, Pavithra <Pavithra_K@intuit.com>
2017-10-25 09:32:37 +0530

    @RestController

--- src/main/java/com/day2/web/PersonController.java
+++ src/main/java/com/day2/web/PersonController.java
@@ -3,15 +3,17 @@ package com.day2.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by pk4 on 10/25/17.
  */
-@Controller
+//@Controller
+@RestController
 public class PersonController {
 
     @RequestMapping("/info")
-    @ResponseBody
+   // @ResponseBody
     public String getInfo()
     {
         return "information";
